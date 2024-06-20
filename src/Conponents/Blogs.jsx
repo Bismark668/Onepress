7db3bb67310afb93514e4ee57bc6063d615e79c0
Bismark668 <147917491+Bismark668@users.noreply.github.com>
2024-06-20 15:51:30 +0000

    Strapi-blogsite

--- src/Conponents/Blogs.jsx
+++ src/Conponents/Blogs.jsx
@@ -45,7 +45,7 @@ export const Blogs = () => {
                         </Link>
                     ))
                 ) : (
-                    <p className='text-center font-bold '>Loading .......</p>
+                    <p className='text-center font-bold '>Loading  blogs .......</p>
                 )}             
             </div>
         </div>  
